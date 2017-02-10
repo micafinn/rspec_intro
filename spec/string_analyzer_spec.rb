79cc042518b641c844881e69100dc108d9880f0d
mlf <micafinn@gmail.com>
2017-02-10 16:07:24 +0200

    true/false/nil matcher

--- spec/string_analyzer_spec.rb
+++ spec/string_analyzer_spec.rb
@@ -80,7 +80,21 @@ describe "An example of the type/class Matchers" do
     expect(x).to be_instance_of Fixnum
     expect(y).to be_kind_of Numeric
     expect(z).to respond_to(:length)
-
   end
+end
 
+describe "An example of the true/false/nil Matchers" do
+  it "should show how the true/false/nil Matchers work" do
+    x = true
+    y = false
+    z = nil
+    a = "testing string"
+
+    #The following expectations will all pass
+    expect(x).to be true
+    expect(y).to be false
+    expect(a).to be_truthy
+    expect(z).to be_falsey
+    expect(z).to be_nil
+    end
 end
