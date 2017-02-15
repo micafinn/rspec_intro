52a7651e861849cf76db15b9f0eab5c32296c5ce
mlf <micafinn@gmail.com>
2017-02-15 09:34:10 +0200

    Add formatter_spec example

---
+++ spec/formatter_spec.rb
@@ -0,0 +1,15 @@
+# RSpec.describe "A spec file to demonstrate how RSpec Formatters work" do
+#   context "when running some tests" do
+#     it "the test usually call the expect() method at least once" do
+#       expect(1 + 1).to eq(2)
+#     end
+#   end
+# end
+
+RSpec.describe "A spec file to demonstrate how RSpec Formatters work" do
+  context "when running some tests" do
+    it "the test usually call the expect() method at least once" do
+      expect(1 + 1).to eq(1)
+    end
+  end
+end
